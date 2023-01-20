8b0ba29470f093a99279624f335263361ec4280f
jteixcppib <123121466+jteixcppib@users.noreply.github.com>
2023-01-20 14:22:50 -0500

    Delete tableautransformer directory

--- tableautransformer/build/lib/tableautransformer/backbone.py
+++
@@ -1,8 +0,0 @@
-# This module is used to restructure data in the shape of any desired graphical output in Tableau
-
-
-def sub_one(number):
-    return number -1
-
-def half(number):
-    return number/2
